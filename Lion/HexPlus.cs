fe0d8f3dce78e6bc5f5fb67cfdb29d84f5dd16a0
Lion Zhou <lionzhou@me.com>
2018-03-17 03:32:20 +0800

    Add summary.

--- Lion/HexPlus.cs
+++ Lion/HexPlus.cs
@@ -3,8 +3,18 @@ using System.Linq;
 
 namespace Lion
 {
+    /// <summary>
+    /// A class deal with Hex
+    /// </summary>
     public class HexPlus
     {
+        #region ByteArrayToHexString
+        /// <summary>
+        /// Convert byte[] to hex string.
+        /// </summary>
+        /// <returns>hex string</returns>
+        /// <param name="_byteArray">byte[]</param>
+        /// <param name="_lower">Is low case</param>
         public static string ByteArrayToHexString(byte[] _byteArray, bool _lower = true)
         {
             string _result = string.Empty;
@@ -18,10 +28,18 @@ namespace Lion
             if (_lower) { _result = _result.ToLower(); }
             return _result;
         }
+        #endregion
 
+        #region HexStringToByteArray
+        /// <summary>
+        /// Convert hex string to byte[]
+        /// </summary>
+        /// <returns>byte[]</returns>
+        /// <param name="_hex">hex string.</param>
         public static byte[] HexStringToByteArray(string _hex)
         {
             return Enumerable.Range(0, _hex.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(_hex.Substring(x, 2), 16)).ToArray();
         }
+        #endregion
     }
 }
