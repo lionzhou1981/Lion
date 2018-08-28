c664610ed856186ce490b5039071cc7f74fddf4c
harvyh1985 <37568699+harvyh1985@users.noreply.github.com>
2018-08-29 00:15:40 +0800

    add addresses check

--- Lion.SDK.Bitcoin/Nodes/Ethereum/Address.cs
+++ Lion.SDK.Bitcoin/Nodes/Ethereum/Address.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace Lion.SDK.Bitcoin.Nodes.Ethereum
@@ -21,6 +22,18 @@ namespace Lion.SDK.Bitcoin.Nodes.Ethereum
         public static Address Parse(string _address)
         {
             return new Address(_address);
+        }
+
+        public static bool IsAddress(string _address)
+        {
+            if (!_address.StartsWith("0x"))
+                return false;
+            var _num64 = _address.Substring(2);
+            BigInteger _valueOf = BigInteger.Zero;
+            if (System.Numerics.BigInteger.TryParse(_num64, System.Globalization.NumberStyles.AllowHexSpecifier, null, out _valueOf))
+                return _valueOf != BigInteger.Zero;
+            else
+                return false;
         }
     }
 }
