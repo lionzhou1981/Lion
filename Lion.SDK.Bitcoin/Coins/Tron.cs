daa6e45d6e7cb3d316345db2fcc0fa4dda05f5e8
harvyh1985 <37568699+harvyh1985@users.noreply.github.com>
2019-02-22 12:27:50 +0800

    Create Tron.cs

---
+++ Lion.SDK.Bitcoin/Coins/Tron.cs
@@ -0,0 +1,28 @@
+﻿using Lion.Encrypt;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lion.SDK.Bitcoin.Coins
+{
+    public class Tron
+    {
+        public static bool IsAddress(string _address)
+        {
+            if (_address.StartsWith("41") && _address.Length == 42)
+                return true;
+            if (!_address.StartsWith("41"))
+            {
+                try
+                {
+                    var _decoded = HexPlus.ByteArrayToHexString(Base58.Decode(_address));
+                    if (_decoded.Length != 50)
+                        return false;
+                }
+                catch { return false; }
+                return true;
+            }
+            return false;
+        }
+    }
+}
