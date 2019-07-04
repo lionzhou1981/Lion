3a1e0c475c6020a690848244952db0bde8f9a6e6
harvyh1985 <37568699+harvyh1985@users.noreply.github.com>
2019-07-04 16:40:14 +0800

    Update MailSender.cs

--- Lion.SDK/Mailgun/MailSender.cs
+++ Lion.SDK/Mailgun/MailSender.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
@@ -9,7 +10,7 @@ namespace Lion.SDK.Mailgun
     {
         string apiKey;
         string domain;
-        public MailSender(string _apiKey,string _domain)
+        public MailSender(string _apiKey, string _domain)
         {
             apiKey = _apiKey;
             domain = _domain;
@@ -20,30 +21,23 @@ namespace Lion.SDK.Mailgun
 
         public bool Send(string _subject, string _from, string _to, string _content)
         {
-            try
+            ServicePointManager.Expect100Continue = false;
+            CredentialCache _credentialCache = new CredentialCache();
+            _credentialCache.Add(new Uri("https://api.mailgun.net/v3"), "Basic", new NetworkCredential("api", ApiKey));
+            Dictionary<string, string> _headers = new Dictionary<string, string>();
+            _headers.Add("key", ApiKey);
+            Dictionary<string, string> _formdata = new Dictionary<string, string>();
+            _formdata.Add("domain", Domain);
+            _formdata.Add("from", _from);
+            _formdata.Add("to", _to);
+            _formdata.Add("subject", _subject);
+            _formdata.Add("text", _content);
+            string _result = "";
+            if (Lion.Net.HttpClient.PostAsFormData("https://api.mailgun.net/v3/mail.isdice.com/messages", _headers, _formdata, out _result, _credentialCache, 120 * 1000))
             {
-                ServicePointManager.Expect100Continue = false;
-                CredentialCache _credentialCache = new CredentialCache();
-                _credentialCache.Add(new Uri("https://api.mailgun.net/v3"), "Basic", new NetworkCredential("api", ApiKey));
-                Dictionary<string, string> _headers = new Dictionary<string, string>();
-                _headers.Add("key", ApiKey);
-                Dictionary<string, string> _formdata = new Dictionary<string, string>();
-                _formdata.Add("domain", Domain);
-                _formdata.Add("from", _from);
-                _formdata.Add("to", _to);
-                _formdata.Add("subject", _subject);
-                _formdata.Add("text", _content);
-                string _result = "";
-                //if (Lion.Net.HttpClient.PostAsFormData("https://api.mailgun.net/v3/mail.isdice.com/messages", _headers, _formdata, out _result, _credentialCache, 120 * 1000))
-                //{
-                //    return !string.IsNullOrWhiteSpace(JObject.Parse(_result)["id"].ToString());
-                //}
-                return false;
+                return !string.IsNullOrWhiteSpace(JObject.Parse(_result)["id"].ToString());
             }
-            catch
-            {
-                return false;
-            }
+            return false;
         }
     }
 }
