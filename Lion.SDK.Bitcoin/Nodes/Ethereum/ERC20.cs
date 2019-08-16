bd0101f3584e0e03a2af48ccef19909e241d5b26
harvyh1985 <harvyh@163.com>
2019-08-16 15:18:01 +0800

    add hex helper,erc20 method

---
+++ Lion.SDK.Bitcoin/Nodes/Ethereum/ERC20.cs
@@ -0,0 +1,12 @@
+namespace Lion.SDK.Bitcoin.Nodes.Ethereum
+{
+    public class ERC20
+    {
+        public const string Method_TotalSupply = "0x18160ddd";
+        public const string Method_BalanceOf = "0x70a08231";
+        public const string Method_Transfer = "0xa9059cbb";
+        public const string Method_TransferFrom = "0x23b872dd";
+        public const string Method_Approve = "0x095ea7b3";
+        public const string Method_Allowance = "0xdd62ed3e";
+    }
+}
--- Lion/HexPlus.cs
+++ Lion/HexPlus.cs
@@ -8,6 +8,17 @@ namespace Lion
     /// </summary>
     public class HexPlus
     {
+        #region HexToInt32 
+
+
+        public static int HexToInt32(string _hex)
+        {
+            _hex = "0" + (_hex.StartsWith("0x", StringComparison.Ordinal) ? _hex.Substring(2) : _hex);
+            return int.Parse(_hex, System.Globalization.NumberStyles.AllowHexSpecifier);
+        }
+
+        #endregion
+
         #region ByteArrayToHexString
         /// <summary>
         /// Convert byte[] to hex string.
@@ -38,37 +49,37 @@ namespace Lion
         /// <param name="_hex">hex string.</param>
         public static byte[] HexStringToByteArray(string _hex)
         {
-    return Enumerable.Range(0, _hex.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(_hex.Substring(x, 2), 16)).ToArray();
-                byte[] bytes = null;
-                if (String.IsNullOrEmpty(_hex))
-                    bytes = new byte[0];
-                else
-                {
-                    int string_length = _hex.Length;
-                    int character_index = (_hex.StartsWith("0x", StringComparison.Ordinal)) ? 2 : 0; // Does the string define leading HEX indicator '0x'. Adjust starting index accordingly.               
-                    int number_of_characters = string_length - character_index;
+            return Enumerable.Range(0, _hex.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(_hex.Substring(x, 2), 16)).ToArray();
+            byte[] bytes = null;
+            if (String.IsNullOrEmpty(_hex))
+                bytes = new byte[0];
+            else
+            {
+                int string_length = _hex.Length;
+                int character_index = (_hex.StartsWith("0x", StringComparison.Ordinal)) ? 2 : 0; // Does the string define leading HEX indicator '0x'. Adjust starting index accordingly.               
+                int number_of_characters = string_length - character_index;
 
-                    bool add_leading_zero = false;
-                    if (0 != (number_of_characters % 2))
-                    {
-                        add_leading_zero = true;
-                        number_of_characters += 1;  // Leading '0' has been striped from the string presentation.
-                    }
-                    bytes = new byte[number_of_characters / 2]; // Initialize our byte array to hold the converted string.
-                    int write_index = 0;
-                    if (add_leading_zero)
-                    {
-                        bytes[write_index++] = FromCharacterToByte(_hex[character_index], character_index);
-                        character_index += 1;
-                    }
-                    for (int read_index = character_index; read_index < _hex.Length; read_index += 2)
-                    {
-                        byte upper = FromCharacterToByte(_hex[read_index], read_index, 4);
-                        byte lower = FromCharacterToByte(_hex[read_index + 1], read_index + 1);
-                        bytes[write_index++] = (byte)(upper | lower);
-                    }
+                bool add_leading_zero = false;
+                if (0 != (number_of_characters % 2))
+                {
+                    add_leading_zero = true;
+                    number_of_characters += 1;  // Leading '0' has been striped from the string presentation.
                 }
-                return bytes;
+                bytes = new byte[number_of_characters / 2]; // Initialize our byte array to hold the converted string.
+                int write_index = 0;
+                if (add_leading_zero)
+                {
+                    bytes[write_index++] = FromCharacterToByte(_hex[character_index], character_index);
+                    character_index += 1;
+                }
+                for (int read_index = character_index; read_index < _hex.Length; read_index += 2)
+                {
+                    byte upper = FromCharacterToByte(_hex[read_index], read_index, 4);
+                    byte lower = FromCharacterToByte(_hex[read_index + 1], read_index + 1);
+                    bytes[write_index++] = (byte)(upper | lower);
+                }
+            }
+            return bytes;
             //return Enumerable.Range(0, _hex.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(_hex.Substring(x, 2), 16)).ToArray();
         }
 
