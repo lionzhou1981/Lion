a5ecfda2eb228e9df6aba945546cb5eea2260652
harvyh1985 <37568699+harvyh1985@users.noreply.github.com>
2022-09-04 22:30:27 +0800

    add rlp decode

---
+++ Lion/Encrypt/RLPItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lion.Encrypt
+{
+    public class RLPItem : IRLPItem
+    {
+        private readonly byte[] data;
+
+        public RLPItem(byte[] _data)
+        {
+            this.data = _data;
+        }
+
+        public byte[] Data
+        {
+            get
+            {
+                return this.Data;
+            }
+        }
+
+    }
+}
