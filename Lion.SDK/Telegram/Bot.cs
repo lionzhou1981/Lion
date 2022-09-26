695f2d1d47ebf44f6b2d2dcc3bbf46e1df610114
harvyh1985 <37568699+harvyh1985@users.noreply.github.com>
2022-09-26 17:51:18 +0800

    Update Bot.cs

--- Lion.SDK/Telegram/Bot.cs
+++ Lion.SDK/Telegram/Bot.cs
@@ -6,6 +6,11 @@ using System.Text;
 
 namespace Lion.SDK.Telegram
 {
+    public class InlineButtonTypes
+    {
+        public const string Url = "url";
+        public const string CallBackButton = "callback_data";
+    }
     public class Bot
     {
         // Fields
@@ -20,8 +25,78 @@ namespace Lion.SDK.Telegram
 
         private string BuildUrl(string _method) =>
             this.urlTemplate.Replace("{{TOKEN}}", this.Token).Replace("{{METHOD_NAME}}", _method);
+        public static string BuildTelegramTable(
+            List<string> _tables,
+            string _tableSep = "|", char _arraySep = ';',
+            int _maxColumnWidth = 0, bool _fixedColumnWidth = false, bool _autoColumnWidth = false,
+            int _minimumColumnWidth = 4, int _columnPadRight = 0, int _columnPadLeft = 0,
+            bool _beginEndBorders = true)
+        {
+            var _preTable = new List<string>() { "<pre>" };
+            var _columnsWidth = new List<int>();
+            var _firstLine = _tables[0];
+            var _lines = _firstLine.Split(_arraySep);
 
-        public JObject AddInlineButtons(JObject _inlineKeyboard, string _text,string url)
+            if (_fixedColumnWidth && _maxColumnWidth == 0) throw new Exception("For fixedColumnWidth usage must set maxColumnWidth > 0");
+            else if (_fixedColumnWidth && _maxColumnWidth > 0)
+            {
+                for (var i = 0; i < _lines.Length; i++)
+                    _columnsWidth.Add(_maxColumnWidth + _columnPadRight + _columnPadLeft);
+            }
+            else
+            {
+                for (var i = 0; i < _lines.Length; i++)
+                {
+                    var _columnData = _lines[i].Trim();
+                    var _columnFullLength = _columnData.Length;
+
+                    if (_autoColumnWidth)
+                        _tables.ForEach(t =>
+                        {
+                            _columnFullLength = t.Split(_arraySep)[i].Length > _columnFullLength ? t.Split(_arraySep)[i].Length : _columnFullLength;
+                        });
+
+                    _columnFullLength = _columnFullLength < _minimumColumnWidth ? _minimumColumnWidth : _columnFullLength;
+                    var columnWidth = _columnFullLength + _columnPadRight + _columnPadLeft;
+                    if (_maxColumnWidth > 0 && columnWidth > _maxColumnWidth)
+                        columnWidth = _maxColumnWidth;
+                    _columnsWidth.Add(columnWidth);
+                }
+            }
+
+            for (int i = 0; i < _tables.Count; i++)
+            {
+                var line = _tables[i];
+                _lines = line.Split(_arraySep);
+
+                var _fullLine = new string[_lines.Length + (_beginEndBorders ? 2 : 0)];
+                if (_beginEndBorders) _fullLine[0] = "";
+
+                for (var j = 0; j < _lines.Length; j++)
+                {
+                    var _data = _lines[j].Trim();
+                    var _dataLength = _data.Length;
+                    var _columnWidth = _columnsWidth[j];
+                    var _columnSizeWithoutTrimSize = _columnWidth - _columnPadRight - _columnPadLeft;
+                    var _dataCharsToRead = _columnSizeWithoutTrimSize > _dataLength ? _dataLength : _columnSizeWithoutTrimSize;
+                    var _columnData = _data.Substring(0, _dataCharsToRead);
+                    _columnData = _columnData.PadRight(_columnData.Length + _columnPadRight);
+                    _columnData = _columnData.PadLeft(_columnData.Length + _columnPadLeft);
+                    var column = _columnData.PadRight(_columnWidth);
+                    _fullLine[j + (_beginEndBorders ? 1 : 0)] = column;
+                }
+
+                if (_beginEndBorders) _fullLine[_fullLine.Length - 1] = "";
+                if (i != 0)
+                    _preTable.Add(string.Join(_tableSep, _fullLine));
+                else
+                    _preTable.Add(string.Join(" ", _fullLine));
+            }
+            _preTable.Add("</pre>");
+            return string.Join("\r\n", _preTable);
+        }
+
+        public JObject AddInlineButtons(JObject _inlineKeyboard, string _text,string _type = "url",string value = "")
         {
             JArray _buttons = new JArray();
             try
@@ -33,7 +108,7 @@ namespace Lion.SDK.Telegram
             _buttons.Add(new JObject()
             {
                 ["text"] = _text,
-                ["url"] = url
+                [_type] = value
             });
             var _setted = new JArray();
             _setted.Add(_buttons);
