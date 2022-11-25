6b739d90b1042d9d106c9765270c09809ce0f40a
harvyh1985 <37568699+harvyh1985@users.noreply.github.com>
2022-11-25 17:38:40 +0800

    Create Utils.cs

---
+++ Lion.CryptoCurrency/Tron/Utils.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lion.CryptoCurrency.Tron
+{
+    public class Utils
+    {
+        public static string DecodeIP(string _ip)
+        {
+            var _len = _ip.Length / 2;
+            var _hex = "42" + _len.ToString("X").ToString().ToLower().PadLeft(2, '0') + _ip;
+            var _info = Lion.CryptoCurrency.Tron.TransactionInfo.NodeInfo.Types.PeerInfo.Parser.ParseFrom(Lion.HexPlus.HexStringToByteArray(_hex));
+            return JObject.Parse(_info.ToString())["host"].ToString();
+        }
+    }
+}
