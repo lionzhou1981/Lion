9715c97a0f48c07776ceada3aa1ca79df2588f53
Lion Zhou <lionzhou@me.com>
2022-12-01 18:05:40 +0800

    两个短信平台接口

---
+++ Lion.SDK/ChuangLan/SMS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Lion.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Lion.SDK.ChuangLan
+{
+    public class SMS
+    {
+        private static string Url = "https://smssh1.253.com/msg/v1/send/json ";
+        public static string Key = "";
+        public static string Secret = "";
+
+        
+        public static bool Send(string _phone,string _text)
+        {
+            JObject _json = new JObject();
+            _json["account"] = Key;
+            _json["password"] = Secret;
+            _json["phone"] = _phone;
+            _json["msg"] = _text;
+            _json["report"] = "true";
+
+            HttpClient _http = new HttpClient(5000);
+            _http.BeginResponse("POST", Url, "");
+            _http.Request.Credentials = new NetworkCredential(Key, Secret);
+            _http.Request.ContentType = "application/json";
+            _http.EndResponse(Encoding.UTF8.GetBytes(_json.ToString(Formatting.None)));
+
+            string _result = _http.GetResponseString(Encoding.UTF8);
+
+            JObject _resultJson = JObject.Parse(_result);
+            return _resultJson.ContainsKey("code") && _resultJson["code"].Value<string>() == "0";
+        }
+    }
+}
---
+++ Lion.SDK/Twilio/SMS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Lion.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Lion.SDK.Twilio
+{
+    public class SMS
+    {
+        private static string Url = "https://api.twilio.com/2010-04-01";
+
+        public static string Account = "";
+        public static string Secret = "";
+        public static string ServiceId = "";
+
+        public static bool Send(string _phone, string _text)
+        {
+            string _data = $"MessagingServiceSid={Uri.EscapeDataString(ServiceId)}&To={Uri.EscapeDataString(_phone)}&Body={Uri.EscapeDataString(_text)}";
+            string _url = $"{Url}/Accounts/{Account}/Messages.json";
+
+            HttpClient _http = new HttpClient(5000);
+            _http.BeginResponse("POST", _url, "");
+            _http.Request.Credentials = new NetworkCredential(Account, Secret);
+            _http.EndResponse(Encoding.UTF8.GetBytes(_data));
+            string _result = _http.GetResponseString(Encoding.UTF8);
+
+            JObject _resultJson = JObject.Parse(_result);
+            return _resultJson.ContainsKey("status") && _resultJson["status"].Value<string>() == "accepted";
+        }
+    }
+}
