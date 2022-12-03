db624272be6734a10fbe443890088e1e3fafbe90
Lion Zhou <lionzhou@me.com>
2022-12-03 08:36:10 +0800

    Add Encode(string)

--- Lion/Encrypt/Base64.cs
+++ Lion/Encrypt/Base64.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Text;
 
 namespace Lion.Encrypt
 {
     public class Base64
     {
+        public static string Encode(string _text) => Convert.ToBase64String(Encoding.UTF8.GetBytes(_text));
+
         public static string Encode(byte[] _byteArray) => Convert.ToBase64String(_byteArray);
 
         public static byte[] Decode(string _base64) => Convert.FromBase64String(_base64);
