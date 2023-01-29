19553486b452d877305d381501616d91de1e1055
Lion Zhou <lionzhou@me.com>
2023-01-29 16:39:11 +0800

    增加固化的参数

--- Lion.CryptoCurrency/Tron/JavaTron.cs
+++ Lion.CryptoCurrency/Tron/JavaTron.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Lion.Net;
+using static Lion.CryptoCurrency.Tron.TransactionInfo.Transaction.Types;
 
 namespace Lion.CryptoCurrency.Tron
 {
@@ -12,11 +13,13 @@ namespace Lion.CryptoCurrency.Tron
     {
         public static bool Debug;
         public static string Host;
+        public static string HostSolidity;
 
         #region Init
-        public static void Init(string _host, bool _debug = false)
+        public static void Init(string _host, string _hostSolidity = "", bool _debug = false)
         {
             Host = _host;
+            HostSolidity = _hostSolidity;
             Debug = _debug;
         }
         #endregion
@@ -33,52 +36,55 @@ namespace Lion.CryptoCurrency.Tron
         #endregion
 
         #region Wallet_GetNowBlock
-        public static JObject Wallet_GetNowBlock()
+        public static JObject Wallet_GetNowBlock(bool _solidity = false)
         {
-            var (Success, Result) = Call("wallet/getnowblock");
+            var (Success, Result) = _solidity ? Call("walletsolidity/getnowblock", null, true) : Call("wallet/getnowblock");
             return Success ? Result : null;
         }
         #endregion
 
         #region Wallet_GetBlockByNumber
-        public static JObject Wallet_GetBlockByNumber(BigInteger _block)
+        public static JObject Wallet_GetBlockByNumber(BigInteger _block, bool _solidity = false)
         {
             JObject _params = new JObject();
             _params["num"] = Int64.Parse(_block.ToString());
-            var (Success, Result) = Call("wallet/getblockbynum", _params);
+
+            var (Success, Result) = _solidity ? Call("walletsolidity/getblockbynum", _params,true) : Call("wallet/getblockbynum", _params);
 
             return Success ? Result : null;
         }
         #endregion
 
         #region Wallet_GetTransactionInfoById
-        public static JObject Wallet_GetTransactionInfoById(string _txid)
+        public static JObject Wallet_GetTransactionInfoById(string _txid, bool _solidity = false)
         {
             JObject _params = new JObject();
             _params["value"] = _txid;
-            var (Success, Result) = Call("wallet/gettransactioninfobyid", _params);
+            var (Success, Result) = _solidity ? Call("walletsolidity/gettransactioninfobyid", _params, true) : Call("wallet/gettransactioninfobyid", _params);
         
             return Success ? Result : null;
         }
         #endregion
 
         #region Wallet_GetTransactionById
-        public static JObject Wallet_GetTransactionById(string _txid)
+        public static JObject Wallet_GetTransactionById(string _txid, bool _solidity = false)
         {
             JObject _params = new JObject();
             _params["value"] = _txid;
-            var (Success, Result) = Call("wallet/gettransactionbyid", _params);
+
+            var (Success, Result) = _solidity ? Call("walletsolidity/gettransactionbyid", _params,true) : Call("wallet/gettransactionbyid", _params);
 
             return Success ? Result : null;
         }
         #endregion
 
         #region Wallet_GetAccount
-        public static JObject Wallet_GetAccount(string _address)
+        public static JObject Wallet_GetAccount(string _address, bool _solidity = false)
         {
             JObject _params = new JObject();
             _params["address"] = _address;
-            var (Success, Result) = Call("wallet/getaccount", _params);
+
+            var (Success, Result) = _solidity ? Call("walletsolidity/getaccount", _params, true) : Call("wallet/getaccount", _params);
 
             return Success ? Result : null;
         }
@@ -115,12 +121,12 @@ namespace Lion.CryptoCurrency.Tron
         #endregion
 
         #region Call
-        public static (bool Success, JObject Result) Call(string _method, JObject _params = null)
+        public static (bool Success, JObject Result) Call(string _method, JObject _params = null, bool _solidity = false)
         {
             try
             {
                 HttpClient _http = new HttpClient(60000);
-                _http.BeginResponse("POST", Host + _method, "");
+                _http.BeginResponse("POST", $"{(_solidity ? HostSolidity : Host)}{_method}", "");
                 if (_params != null)
                 {
                     _http.EndResponse(Encoding.UTF8.GetBytes(_params.ToString(Formatting.None)));
@@ -134,7 +140,7 @@ namespace Lion.CryptoCurrency.Tron
 
                 return (true, JObject.Parse(_result));
             }
-            catch(Exception _ex)
+            catch (Exception _ex)
             {
                 Console.WriteLine(_ex);
                 return (false, new JObject() { ["error"] = _ex.ToString() });
