ac3c5593696bd0726707c362356def3f47df0cca
harvyh1985 <37568699+harvyh1985@users.noreply.github.com>
2023-07-10 16:24:36 +0800

    新增smtp邮件发送/geetest

---
+++ Lion.SDK/GeeTest/GeeTest.cs
@@ -0,0 +1,43 @@
+﻿using Lion.Net;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lion.SDK.GeeTest
+{
+    public static class GeeTest
+    {
+        static string Id = "";
+        static string Key = "";
+        const string ApiServer = "http://gcaptcha4.geetest.com/validate?captcha_id=";
+        public static void Init(string _id,string _key)
+        {
+            Id = _id;
+            Key = _key;
+        }
+
+        public static bool Test(string _lotNumber,string _captchaOutput,string _passToken,string _genTime)
+        {
+            using var _hmacsha256 = new HMACSHA256(UTF8Encoding.UTF8.GetBytes(Key));
+            byte[] _signed = _hmacsha256.ComputeHash(UTF8Encoding.UTF8.GetBytes(_lotNumber));
+            var _form = new Dictionary<string, string>();
+            _form["lot_number"] = _lotNumber;
+            _form["captcha_output"] = _captchaOutput;
+            _form["pass_token"] = _passToken;
+            _form["gen_time"] = _genTime;
+            _form["sign_token"] = HexPlus.ByteArrayToHexString(_signed);
+            try
+            {
+                HttpClient.PostAsFormData(ApiServer + Id, new Dictionary<string, string>(), _form, out string _resp);
+                var _re = JObject.Parse(_resp);
+                return _re["result"].ToString() == "success";
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Lion.SDK/SmtpMail/SmtpMail.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Lion.SDK.SmtpMail
+{
+    public static class SmtpMail
+    {
+        static string User;
+        static string Password;
+        static string Host;
+        static int Port;
+        public static void Init(string _user,string _password,string _host,int _port)
+        {
+            User = _user;
+            Password = _password;
+            Host = _host;
+            Port = _port;
+        }
+
+        public static bool Send(string _senderName, string _receiver, string _title, string _htmlBody)
+        {
+            try
+            {
+                SmtpClient _client = new SmtpClient();
+                _client.Host = Host;
+                _client.Port = Port;
+                _client.Credentials = new NetworkCredential(User, Password);
+                _client.Send(new MailMessage(User, _receiver)
+                {
+                    Sender = new MailAddress(User, _senderName),
+                    Body = _htmlBody,
+                    IsBodyHtml = true,
+                    Subject = _title,
+                    BodyEncoding = System.Text.Encoding.UTF8,
+                    SubjectEncoding = System.Text.Encoding.UTF8,
+                });
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Smtp Mail send error:" + ex.Message + "|" + ex.StackTrace);
+                return false;
+            }
+        }
+
+    }
+}
